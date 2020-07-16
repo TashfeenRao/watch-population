34976dea566b416aea2289cff49f4d976662e864
tashfeenrao <raotashfeen25@gmail.com>
2020-07-16 17:59:27 +0500

    created directories for redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './reducers/index';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App store={store} />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { createStore, combineReducers } from 'redux';
+
+const reducers = combineReducers({
+})
+
+const store = createStore(reducers);
+
+export default store;
