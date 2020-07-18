96fdfdd6fd3fdc82dd29ca098259d0261f781fc3
tashfeenrao <raotashfeen25@gmail.com>
2020-07-19 03:12:56 +0500

    filter by currency logic added

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,18 +4,33 @@
 /* eslint-disable react/no-array-index-key */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import filterProducts from '../actions/filterProducts';
 
 class Products extends Component {
+  constructor(props) {
+    super(props);
+    this.filterCurrencies = this.filterCurrencies.bind(this);
+  }
+
+  filterCurrencies(e) {
+    const { dispatch } = this.props;
+    dispatch(filterProducts(e.target.value));
+  }
+
   render() {
     const {
-      products,
+      products, filters,
     } = this.props;
+    console.log();
+    const prodArray = Object.entries(products);
+    const filteredProducts = () => ((filters !== '') ? prodArray.filter(prod => prod[0] === filters.toUpperCase()) : prodArray);
     return (
       <div>
+        <input type="text" name="input" onChange={e => this.filterCurrencies(e)} />
         <ul>
-          {Object.entries(products).map((item, index) => (
+          {filteredProducts().map((item, index) => (
             <div key={index}>
-              <li>{item}</li>
+              <li>{item[0]}</li>
             </div>
           ))}
         </ul>
@@ -25,4 +40,5 @@ class Products extends Component {
 }
 export default connect(state => ({
   products: state.products,
+  filters: state.input,
 }))(Products);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { productReducer } from './productReducer';
 import loading from './loading';
-import countryReducer from './countryReducer'
+import countryReducer from './countryReducer';
+import filterReducer from './filterReducer';
 
 export default combineReducers({
   products: productReducer,
   loading,
   countries: countryReducer,
+  input: filterReducer,
 });
