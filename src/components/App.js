5caab6abcb184df310bcc9c57130c622d4fda7af
tashfeenrao <raotashfeen25@gmail.com>
2020-07-22 03:23:18 +0500

    spinner added

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Products from './Products';
 import fetchProducts from '../actions/fetchProducts';
+import '../style/product.css';
 
 class App extends Component {
   componentDidMount() {
@@ -15,10 +16,12 @@ class App extends Component {
     const { pending, error } = this.props;
     if (pending === true || error === true) {
       return (
-        <h1>
-          Loading
-          {error}
-        </h1>
+        <div className="text-center">
+          <div className="spinner-grow spinner" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+
       );
     }
     return (
